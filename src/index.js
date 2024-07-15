a661a87a21f44a820677160832baabf615a8075e
yuvath <13.bscs.147@gmail.com>
2024-07-15 15:52:36 +0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import ReactDom from "react-dom/client";
+// import "./index.css";
+// import App from "./components/App";
+import StarRating from "./components/StarRating";
+
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+
+  return (
+    <div>
+      <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
+      <p>This movie was rated {movieRating} stars</p>
+    </div>
+  );
+}
+
+const root = ReactDom.createRoot(document.getElementById(`root`));
+root.render(
+  <React.StrictMode>
+    {/* <App /> */}
+    <StarRating
+      maxRating={5}
+      message={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+      defaultRating={3}
+    />
+    <StarRating
+      maxRating={5}
+      size={24}
+      color="red"
+      className="test"
+      defaultRating={3}
+    />
+    <Test />
+  </React.StrictMode>
+);
